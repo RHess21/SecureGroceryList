2e4c80f9bd9ecdb2ef0f3ded1568cf6cc24ea358
Ryan Hess <rhess1621@gmail.com>
2024-03-19 10:59:30 -0400

    Changed the name of the patterns in InputCheck.py

--- InputCheck.py
+++ InputCheck.py
@@ -6,20 +6,20 @@ def check_input(email, password, first_name, last_name, label_result):
    
 # Regular expression pattern for password security checks
 
-    pattern1 = r"^([A-Za-z0-9]+)@([A-Za-z0-9]+)\.([A-Za-z]{2,})$" #Email pattern
-    pattern2 = r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"          #Password pattern
-    pattern3 = r"^([A-Za-z]+)$"                                      #Name pattern
+    emailPat = r"^([A-Za-z0-9]+)@([A-Za-z0-9]+)\.([A-Za-z]{2,})$" #Email pattern
+    passPat = r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"          #Password pattern
+    namePat = r"^([A-Za-z]+)$"                                      #Name pattern
 
-    if not re.match(pattern1, email):
+    if not re.match(emailPat, email):
             label_result.config(text="Invalid email!", fg="red")
             return False
-    elif not re.match(pattern2, password): 
+    elif not re.match(passPat, password): 
             label_result.config(text="Password must be at least 8 characters long and contain at least one letter and one number!", fg="red")
             return False
-    elif not re.match(pattern3, first_name):
+    elif not re.match(namePat, first_name):
             label_result.config(text="First name must only contain letters!", fg="red")
             return False
-    elif not re.match(pattern3, last_name):
+    elif not re.match(namePat, last_name):
             label_result.config(text="Last name must only contain letters!", fg="red")
             return False
     else:
